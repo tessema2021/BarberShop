7c4d0bf66f81d93fedb1b035770d8aaf7f6da795
tessema2021 <ninafuchere@gmail.com>
2022-04-08 15:57:36 -0500

    adding payment method still working on it

---
+++ BarberShop/Repositories/PaymentRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BarberShop.Repositories
+{
+    public class PaymentRepositorycs
+    {
+    }
+}
